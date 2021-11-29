870b275babf40d8a862ef5b5bb2248119397570f
Nair Suraj Gangadharan <nsgarafaite@gmail.com>
2021-11-29 21:20:39 +0530

    added mui arrow

--- leave-planner/src/components/Dropdown.js
+++ leave-planner/src/components/Dropdown.js
@@ -3,11 +3,14 @@ import './Dropdown.css'
 function Dropdown({ selected, setSelected }) {
   const [isActive, setIsActive] = useState(false);
   const options = ["Sick", "Casual", "Annual","Maternity/Paternity","Personal"];
+
   return (
     <div className="dropdown">
       <div className="dropdown-btn" onClick={(e) => setIsActive(!isActive)}>
         {selected}
-        <span className="fas fa-caret-down"></span>
+        <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2">
+                   expand_more
+                </span>
       </div>
       {isActive && (
         <div className="dropdown-content">
